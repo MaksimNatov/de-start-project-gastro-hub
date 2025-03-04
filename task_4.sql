b2426afefcfff38a97ff3b956a2069bb8b811ffa
MaksimNatov <maksimnatov@gmail.com>
2025-03-04 15:41:02 +0300

    Update task_4.sql
    Первая итерация

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,28 @@
 /*добавьте сюда запрос для решения задания 4*/
+WITH 
+fk AS (
+SELECT 
+	restaurant_name
+	,(jsonb_each(menu)).KEY AS f_key
+	,(jsonb_each(menu)).value::jsonb AS f_val
+FROM cafe.restaurants),
+sk AS (
+SELECT 
+	fk.restaurant_name
+	,jsonb_each_text(f_val) AS pizza
+FROM fk
+WHERE f_key ILIKE 'Пицца'),
+pred_res AS (
+SELECT
+	sk.restaurant_name
+	,COUNT(sk.pizza) AS pizza_count
+	,DENSE_RANK() OVER (ORDER BY (COUNT(sk.pizza)) desc) AS rank
+FROM sk
+GROUP BY 1
+ORDER BY 2 DESC)
+SELECT
+	pred_res.restaurant_name,
+	pred_res.pizza_count
+FROM pred_res
+WHERE
+	pred_res.RANK = 1;
