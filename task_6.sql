76a52b69e1ee8ccc9979f59a725bbf33573a8141
MaksimNatov <maksimnatov@gmail.com>
2025-03-04 15:41:47 +0300

    Update task_6.sql
    Первая итерация

--- task_6.sql
+++ task_6.sql
@@ -1,1 +1,16 @@
 /*добавьте сюда запросы для решения задания 6*/
+BEGIN;
+WITH res as(
+SELECT 
+cafe.restaurants.restaurant_uuid
+,to_jsonb(((menu #>> '{Кофе, Капучино}') :: int * 1.2) :: int) AS val
+FROM cafe.restaurants 
+WHERE 1=1
+	AND menu #>> '{Кофе, Капучино}' IS NOT NULL
+FOR NO KEY UPDATE) --блокируем строки, пока транзакция не завершится, никто не сможет изменить или удалить строки
+UPDATE 	cafe.restaurants AS r
+	SET
+		menu = jsonb_set(menu, '{Кофе, Капучино}', res.val)
+	FROM res
+	WHERE res.restaurant_uuid = r.restaurant_uuid;
+COMMIT;
